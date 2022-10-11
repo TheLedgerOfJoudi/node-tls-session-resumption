b84e11f9452d04e5a91184d4e2efe7639a5ed2d2
Karam19 <karam.shbeb1@hotmail.com>
2022-10-11 23:17:52 +0300

    fixed reject unauthorized

--- src/client.ts
+++ src/client.ts
@@ -1,41 +1,58 @@
-import tls = require('tls')
-import fs = require('fs')
-import path = require('path')
+import tls = require("tls");
+import fs = require("fs");
+import path = require("path");
 
+let counter = 0;
 
-export const make_client = async (port: number) => {
-    var session_buffer: Buffer | undefined;
-    var resumed: boolean = false;
+export const make_client = new Promise<boolean>((resolve: any, reject: any) => {
+  var session_buffer: Buffer | undefined;
+  let port = 8000
+//   var resumed: boolean = false;
+  const options: tls.ConnectionOptions = {
+    port: port,
+    rejectUnauthorized: false,
+    key: fs.readFileSync(
+      path.normalize(__dirname + "/../cert" + "/private-key.pem")
+    ),
+    cert: fs.readFileSync(
+      path.normalize(__dirname + "/../cert" + "/public-cert.pem")
+    ),
+  };
+
+  var conn: tls.TLSSocket = tls.connect(options, function () {
+    if (conn.authorized) {
+      console.log("Connection authorized by a Certificate Authority.");
+    } else {
+      console.log("Connection not authorized: " + conn.authorizationError);
+    }
+    console.log();
+  });
+
+  conn.on("session", (session: Buffer | undefined) => {
+    session_buffer = session;
+    conn.end();
     const options: tls.ConnectionOptions = {
-        port: port,
-        key: fs.readFileSync(path.normalize(__dirname + '/../cert' + '/private-key.pem')),
-        cert: fs.readFileSync(path.normalize(__dirname + '/../cert' + '/public-cert.pem')),
+      port: port,
+      rejectUnauthorized: false,
+      session: session_buffer,
+      key: fs.readFileSync(
+        path.normalize(__dirname + "/../cert" + "/private-key.pem")
+      ),
+      cert: fs.readFileSync(
+        path.normalize(__dirname + "/../cert" + "/public-cert.pem")
+      ),
     };
-
-    var conn: tls.TLSSocket = tls.connect(options, function () {
-        if (conn.authorized) {
-            console.log("Connection authorized by a Certificate Authority.");
-        } else {
-            console.log("Connection not authorized: " + conn.authorizationError)
-        }
-        console.log();
+    var conn2: tls.TLSSocket = tls.connect(options, function () {
+      console.log("resumed");
+      counter += 1
+      console.log('counter is', counter);
+      if (counter == 2){
+        console.log('ended');
+        resolve(true);
+        conn2.end();
+        conn2.destroy();
+        conn.destroy();
+      }
     });
-
-    conn.on('session', (session: Buffer | undefined) => {
-        session_buffer = session;
-        conn.end();
-        const options: tls.ConnectionOptions = {
-            port: port,
-            session: session_buffer,
-            key: fs.readFileSync(path.normalize(__dirname + '/../cert' + '/private-key.pem')),
-            cert: fs.readFileSync(path.normalize(__dirname + '/../cert' + '/public-cert.pem')),
-        };
-        tls.connect(options, function () {
-            console.log("resumed")
-            resumed = true
-        });
-    });
-    return resumed
-
-}
-
+  });
+});
--- test/client.test.ts
+++ test/client.test.ts
@@ -1,7 +1,10 @@
+import { make_client } from "../src/client";
 
-import {make_client} from '../src/client'
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 test("Client test", async () => {
-  const resumption_state = await make_client(8000);
+  const resumption_state = await make_client;
+  console.log('res state is: ',resumption_state)
+  // expect(resumption_state).resolves.toEqual(true);
   expect(resumption_state).toBe(true);
 });
